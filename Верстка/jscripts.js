f20c874b042c6e1292b7181aaae1b1151fefa9aa
SosiskaMann <bahr.sen@gmail.com>
2022-12-07 20:48:46 +0300

    1stProject

---
+++ Верстка/jscripts.js
@@ -0,0 +1,6 @@
+const workButton = document.querySelector(".item-about__title__button");
+const text = documet.querySelector(".item-about__text");
+addEventListener("click", () =>{
+    text.classList.toggle("invisible");
+})  
+const divMaker = document.querySelector(".item-abput__text");
